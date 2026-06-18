289bcbb840a61241e5cd5ed91bb70f9fd6e502e1
deploy sla <joaopires3642@gmail.com>
2026-06-17 22:47:38 -0300

    fix: move eslint-disable comment before setItems

--- src/context/wishlist-context.tsx
+++ src/context/wishlist-context.tsx
@@ -44,8 +44,8 @@ export function WishlistProvider({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     if (!isLoaded) return
-    setItems(readStoredWishlist(storageKey))
     // eslint-disable-next-line react-hooks/set-state-in-effect
+    setItems(readStoredWishlist(storageKey))
   }, [isLoaded, storageKey])
 
   useEffect(() => {
