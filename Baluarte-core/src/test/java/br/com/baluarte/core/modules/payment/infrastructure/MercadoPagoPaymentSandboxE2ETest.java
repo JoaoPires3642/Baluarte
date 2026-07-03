a3979ea8896db9d3064a91e09b9a82fc1f27bb3e
deploy sla <joaopires3642@gmail.com>
2026-07-02 23:51:51 -0300

    fix: remover importação desnecessária do MockBean no teste MercadoPagoPaymentSandboxE2ETest

--- Baluarte-core/src/test/java/br/com/baluarte/core/modules/payment/infrastructure/MercadoPagoPaymentSandboxE2ETest.java
+++ Baluarte-core/src/test/java/br/com/baluarte/core/modules/payment/infrastructure/MercadoPagoPaymentSandboxE2ETest.java
@@ -37,7 +37,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
